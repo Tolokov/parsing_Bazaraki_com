1a56fd3aed563827eab8c917da3d873689ad6da1
Tolokov <vitaliitolokov@gmail.com>
2022-05-10 14:14:56 +0500

    Update output Дубликатов: 2 Товаров: 4100 Время выполнения: 0:12:32

--- main.py
+++ main.py
@@ -2,26 +2,39 @@ from requests import get
 from json import dump
 from math import ceil
 
+from datetime import datetime
+
+
+def timer(f):
+    def dec(*args, **kwargs):
+        start = datetime.now()
+        result = f(*args, **kwargs)
+        end = datetime.now()
+        print('Времени затрачено на выполнение скрипта: ', end - start)
+        return result
+    return dec
+
 
 def get_count_items(url: str) -> int:
     """Получение ответа от сервиса"""
     response = get(url)
     if response.status_code == 200:
-        print(f'Found {response.json()["count"]} items')
         pages = response.json()["count"] / 10
-        return ceil(pages)
+        total_pages = ceil(pages)
+        print(f'Found {response.json()["count"]} items and {total_pages} pages')
+        return total_pages
     else:
         print(f'{url} does not exist')
         raise StopIteration('The site is not available or the address has changed')
 
 
-def get_items(url:str, number_of_pages:int):
+def get_items(url:str, pages:int):
     """Парсинг содержимого, и разбиение на блоки с товарами"""
     items = dict()
     count_duplicate = 0
-    for number_of_pages in range(1, number_of_pages + 1):
+    for number_of_pages in range(1, pages + 1):
         link = url + f'&page={number_of_pages}'
-        print('.', sep='')
+        print(f'Loading page # {number_of_pages}/{pages}')
         response = get(link)
         if response.status_code == 200:
             api_answer = response.json()["results"]
@@ -38,12 +51,13 @@ def get_items(url:str, number_of_pages:int):
     return items
 
 
+@timer
 def main():
     rubric = 5
     url = f'https://www.bazaraki.com/api/items/?rubric={rubric}'
 
-    number_of_pages = get_count_items(url)
-    items = get_items(url, number_of_pages)
+    pages = get_count_items(url)
+    items = get_items(url, pages)
 
     with open('content_from_Bazaraki.json', 'w') as f_json:
         dump(items, f_json, indent=4)
